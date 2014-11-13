31e2450c9d91a1b7e10e87348331755aa2b29589
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-11-12 22:10:23 -0300

    Codebase for index.js.

---
+++ server/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
+
+server.listen(8000);
+
+// Serving webapps
+app.use('/admin', express.static(path.join(__dirname, '../public/admin-app')));
+app.use('/presentation', express.static(path.join(__dirname, '../public/presentation-app')));
+
+// socket.io server
+io.on('connection', function (socket) {
+	console.log('New connection.');
+});
