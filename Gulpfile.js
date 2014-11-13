196f641ce39682c85aaeb6339f5b5e38e05633bf
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-11-13 12:54:00 -0300

    Added test tasks to gulpfile. Modified .travis.yml to run gulp.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var karma = require('gulp-karma');
 
 gulp.task('lint', function () {
 	return
@@ -10,10 +11,25 @@ gulp.task('lint', function () {
     .pipe(jshint())
 })
 
-gulp.task('test', function () {
+gulp.task('test-backend', function () {
     return gulp.src('server/tests/*.js', {read: false})
         .pipe(mocha({reporter: 'spec'}));
 });
+
+gulp.task('test-frontend', function() {
+  // Be sure to return the stream
+  return gulp.src('public/*/tests/*.js')
+    .pipe(karma({
+      configFile: 'karma.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+gulp.task('test', ['test-backend', 'test-frontend'])
 
 gulp.task('develop', function () {
   nodemon({ script: 'server/index.js', ext: 'js' })
@@ -22,3 +38,5 @@ gulp.task('develop', function () {
       console.log('restarted!')
     })
 })
+
+gulp.task('default', ['test'])
