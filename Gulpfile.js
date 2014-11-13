fc87c592efe3ed933b32d15429a01ea0ed6d4fcc
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-11-13 00:41:56 -0300

    Added codebase for server testing with mocha.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,19 @@
 // Gulpfile.js
-var gulp = require('gulp')
-  , nodemon = require('gulp-nodemon')
-  , jshint = require('gulp-jshint')
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
 
 gulp.task('lint', function () {
 	return
   gulp.src('./**/*.js')
     .pipe(jshint())
 })
+
+gulp.task('test', function () {
+    return gulp.src('server/tests/*.js', {read: false})
+        .pipe(mocha({reporter: 'spec'}));
+});
 
 gulp.task('develop', function () {
   nodemon({ script: 'server/index.js', ext: 'js' })
