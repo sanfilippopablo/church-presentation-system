4d041e4d52f8d00f4d5601fd912e3dc6dcbe140d
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-11-19 17:48:42 -0300

    Changed url for live view. Also deleted dummy code.

--- server/index.js
+++ server/index.js
@@ -8,14 +8,9 @@ server.listen(8000);
 
 // Serving webapps
 app.use('/admin', express.static(path.join(__dirname, '../public/admin-app')));
-app.use('/presentation', express.static(path.join(__dirname, '../public/presentation-app')));
+app.use('/live', express.static(path.join(__dirname, '../public/presentation-app')));
 
 // socket.io server
 io.on('connection', function (socket) {
-	console.log('New connection.');
 
-	socket.on('echo', function(data){
-		console.log("Echo received")
-		socket.emit('response', data)
-	})
 });
