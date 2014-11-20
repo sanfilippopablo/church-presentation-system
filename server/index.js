9d495270814435ce8ae65e0daf8012614567fd9e
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-11-20 02:33:18 -0300

    Some base work on frontend.

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var path = require('path');
 
-server.listen(8000);
+server.listen(80);
 
 // Serving webapps
-app.use('/admin', express.static(path.join(__dirname, '../public/admin-app')));
-app.use('/live', express.static(path.join(__dirname, '../public/presentation-app')));
+app.use('/admin', express.static(path.join(__dirname, '../public/admin')));
+app.use('/live', express.static(path.join(__dirname, '../public/live')));
+app.use('/libs', express.static(path.join(__dirname, '../public/libs')));
 
 // socket.io server
 io.on('connection', function (socket) {
