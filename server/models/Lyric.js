1192b56ae0f258fe62d2b72b07e249d57f969f42
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-11-20 01:09:16 -0300

    Added mongodb to .travis.yml. New test. New method on Lyric.

--- server/models/Lyric.js
+++ server/models/Lyric.js
@@ -1,6 +1,7 @@
 module.exports = function (mongoose) {
   var Schema = mongoose.Schema;
 
+  // Create Schema
   var lyricSchema = new Schema({
     title: String,
     creationDate: {type: Date, default: Date.now},
@@ -8,12 +9,22 @@ module.exports = function (mongoose) {
       type: {type: String},
       lines: [String]
     }]
-  })
+  });
 
+  // Add indexes
+  lyricSchema.index(
+    {'verses.lines': 'text'},
+    { 'default_language': 'spanish' }
+  )
+
+  // Static Methods
   lyricSchema.statics.query = function (str, cb) {
     this.find({$text: {$search: str}}, cb)
   }
 
+  // Instance Methods
+
+  // Create model
   var Lyric = mongoose.model('Lyric', lyricSchema);
   return Lyric;
 }
--- server/tests/lyricsModel.js
+++ server/tests/lyricsModel.js
@@ -9,12 +9,20 @@ describe('Lyric model', function(){
 
   describe('query() method', function(){
 
-    it('should return null if no query', function(done){
+    it('should return empty array if no query', function(done){
       Lyric.query("", function(err, docs){
-        assert.equal(docs, null);
-        done();
+        docs.length.should.equal(0);
+        done(err);
       })
     });
-    
+
+    it('should return the song i want', function(done){
+      Lyric.query('fidelidad', function(err, docs){
+        assert.equal(docs.length, 1);
+        assert.equal(docs[0].id, '546d3e579a308c810513d648');
+        done(err);
+      })
+    })
+
   })
 })
