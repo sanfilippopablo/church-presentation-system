821c750f942358f57298194a360608903f75c00d
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-11-27 01:39:53 -0300

    Added base for routing.

--- public/admin/scripts/app.js
+++ public/admin/scripts/app.js
@@ -1,3 +1,23 @@
-var app = angular.module('app', [
-  'ngMaterial'
-])
+angular
+  .module('app', [
+    'ngMaterial',
+    'ui.router'
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+    .state('songs', {
+      url: '/songs',
+      views: {
+        'primary-menu': {
+          templateUrl: 'partials/songs/primary-menu.html'
+        },
+        'secondary-menu': {
+          templateUrl: 'partials/songs/secondary-menu.html'
+        }
+      }
+    });
+
+
+    $urlRouterProvider.otherwise('/songs');
+  });
