d813c033c8da763f1b815d6a3e6b2988ade6a235
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2014-11-27 01:21:01 -0300

    Separated socket code into a new file.

---
+++ server/controllers/Song.js
@@ -0,0 +1,31 @@
+module.exports = function(socket, Song) {
+
+  // Create
+  socket.on('song:create', function(song) {
+    console.log(song)
+    Song.create(song, function(err, obj) {
+      socket.emit('song:created', obj);
+    })
+  });
+
+  // Update
+  socket.on('song:update', function(id, data) {
+    Song.findByIdAndUpdate(id, data, function(err, obj) {
+      socket.emit('song:updated', obj);
+    })
+  });
+
+  // Delete
+  socket.on('song:delete', function(id) {
+    Song.findByIdAndRemove(id, function(err, obj) {
+      socket.emit('song:deleted', obj);
+    })
+  });
+
+  // Query
+  socket.on('song:query', function(text) {
+    Song.query(text, function(err, songs) {
+      socket.emit('song:queryresult', songs);
+    })
+  });
+}
--- server/index.js
+++ server/index.js
@@ -21,33 +21,7 @@ app.use('/libs', express.static(path.join(__dirname, '../public/libs')));
 // socket.io server
 io.on('connection', function (socket) {
 
-  // SONGS //
-  // Create
-  socket.on('song:create', function(song) {
-    Song.create(song, function(err, obj){
-      socket.emit('song:created', obj);
-    })
-  });
-
-  // Update
-  socket.on('song:update', function(id, data) {
-    Song.findByIdAndUpdate(id, data, function(err, obj){
-      socket.emit('song:updated', obj);
-    })
-  });
-
-  // Delete
-  socket.on('song:delete', function(id) {
-    Song.findByIdAndRemove(id, function(err, obj){
-      socket.emit('song:deleted', obj);
-    })
-  });
-
-  // Query
-  socket.on('song:query', function(text) {
-    Song.query(text, function(err, songs){
-      socket.emit('song:queryresult', songs);
-    })
-  });
+  // Songs
+  require('./controllers/Song')(socket, Song);
 
 });
